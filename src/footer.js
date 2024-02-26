6abce9788e0b1d0088decd6efb325ab2f1b5a600
Josh Iyinkanmi <89524882+GwinzTV@users.noreply.github.com>
2024-02-26 23:44:07 +0000

    first commit

---
+++ src/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Styles.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 GwinzTV Developments</p>
+    </footer>
+  );
+}
+
+export default Footer;
